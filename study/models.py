896b77930af21b65cf8aa65b69fe557a8783a11d
YKNNagi <kyk.main@gmail.com>
2026-08-22 14:41:11 +0900

    feat: タグモデルとStudyの多対多関連を追加

--- study/models.py
+++ study/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.conf import settings
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
 class Study(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete = models.CASCADE
     )
     content = models.CharField(max_length=200)
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True
+        )
     created_at = models.DateTimeField(auto_now_add=True)
