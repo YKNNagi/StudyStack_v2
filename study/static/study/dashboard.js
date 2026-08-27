fdda6343ecd62b743852518469b4f7d58a4c916f
YKNNagi <kyk.main@gmail.com>
2026-08-27 12:54:00 +0900

    Add tag selection limit control

--- study/static/study/dashboard.js
+++ study/static/study/dashboard.js
@@ -1,7 +1,22 @@
 const tagSelect = document.getElementById("id_tags");
 const selectedTags = document.getElementById("selected-tags");
 
+let previousSelectedOptions = Array.from(tagSelect.selectedOptions);
+
 tagSelect.addEventListener("change", function() {
+
+    console.log(tagSelect.selectedOptions.length);
+    if (tagSelect.selectedOptions.length > 2) {
+
+    for (const option of tagSelect.options) {
+        option.selected = previousSelectedOptions.includes(option);
+    }
+
+    } else {
+
+        previousSelectedOptions = Array.from(tagSelect.selectedOptions);
+
+    }
 
     selectedTags.innerHTML = "";
 
